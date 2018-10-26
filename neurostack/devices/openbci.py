aeb3cbdccd1ef7b2221ca93ad28541dd0969add3
Sayan Faraz <sayan96@hotmail.com>
2018-10-25 20:05:50 -0400

    Adding device functionality for OpenBCIPython + requirements

--- neurostack/devices/openbci.py
+++ neurostack/devices/openbci.py
@@ -1,23 +1,72 @@
+import openbci
+
 from devices.device import Device
-import usb
 
 
 class OpenBCI(Device):
 
     def __init__(self, device_id=None):
-        pass
+        super().__init__(device_id)
 
-    def available_devices(self):
-        """ Returns list of available OpenBCI devices by device name 
+    @staticmethod
+    def available_devices():
+        """
+        Returns list of available OpenBCI devices by device name
         
         Works on Linux, untested for other OS
         """
-        devs = usb.core.find(find_all=True, idVendor=0x0403, idProduct=0x6015)
-        openBCI_devices = []
+        # devs = usb.core.find(find_all=True, idVendor=0x0403, idProduct=0x6015)
+        # openBCI_devices = []
 
-        for device in devs:
-            if device.manufacturer == "FTDI":   # must be run with sudo to see manufacturer
-                openBCI_devices.append(device.product) # append USB name
+        # for device in devs:
+        #     if device.manufacturer == "FTDI":   # must be run with sudo to see manufacturer
+        #         openBCI_devices.append(device.product) # append USB name
 
-        return openBCI_devices
+        # return openBCI_devices
 
+        pass  # OpenBCICyton does it internally
+
+    def connect(self, device_id=None):
+        """
+        Connect to EEG device with id specified. If id is not specified,
+        connect to randomly selected EEG device.
+
+        :param device_id:
+        :return:
+        """
+        self.openbci_cyton = openbci.OpenBCICyton()
+
+    def start(self) -> None:
+        """
+        Start streaming EEG from device, and publish data to subscribers.
+    
+        :return:
+        """
+        self.openbci_cyton.start_streaming(callback)
+
+    def stop(self) -> None:
+        """
+        Stop streaming EEG data from device, and stop publishing data to
+        subscribers. Connection to device remains intact, and device is not
+        turned off.
+    
+        :return:
+        """
+        self.openbci_cyton.stop()
+
+    def shutdown(self) -> None:
+        """
+        Close connection to device, WebSocket connections to publishers, and tag
+        sources.
+    
+        :return:
+        """
+        self.openbci_cyton.disconnect()
+
+    def get_info(self) -> None:
+        """
+        Get information about the device including device type, connection type, electrode names, device type, stream time, etc... 
+    
+        :return:
+        """
+        pass
