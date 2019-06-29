3b002e84c3cb59633ddb8f231a6da2ed0a5bb479
Karl Cui <karl.cui@mail.utoronto.ca>
2019-06-28 23:20:26 -0400

    Muse device

--- neurostack/devices/device.py
+++ neurostack/devices/device.py
@@ -3,11 +3,8 @@ from abc import ABC, abstractmethod
 
 class Device(ABC):
 
-    def __init__(self, device_id=None, connect=True):
+    def __init__(self, device_id=None):
         self.device_id = device_id
-
-        if connect:
-            self.connect(device_id)
 
     @abstractmethod
     @staticmethod
@@ -63,7 +60,7 @@ class Device(ABC):
     @abstractmethod
     def get_info(self) -> None:
         """
-        Get information about the device including device type, connection type, electrode names, device type, stream time, etc... 
+        Get information about the device including device type, connection type, electrode names, device type, stream time, etc...
 
         :return:
         """
--- neurostack/devices/muse.py
+++ neurostack/devices/muse.py
@@ -1,7 +1,44 @@
+from data_streams import EEG_stream
 from devices import Device
+
+import warnings
 
 
 class Muse(Device):
 
     def __init__(self, device_id=None):
+        super().__init__(device_id=device_id)
+        self.streams = {}
+
+    #
+    # Private device methods for handling data streams and server connections
+    #
+
+    def _create_eeg_stream():
+        return EEGStream(thread_name='EEG_data', event_channel_name='P300')
+
+    #
+    # Public device metods
+    #
+
+    def connect(self, device_id=None):
+        """ Creates an EEG stream if there are none and connects to it. """
+        if self.streams.get('eeg') is None:
+            self.streams['eeg'] = self._create_eeg_stream()
+
+        self.streams['eeg'].lsl_connect()
+
+    def start(self):
+        """ Start streaming EEG data """
+        self.streams['eeg'].start()
+
+    def stop(self):
+        """ Stop streaming EEG data """
+        self.streams['eeg'].stop()
+
+    def shutdown(self):
+        """ Disconnect EEG stream (and stop streaming data) """
+        self.streams['eeg'] = None
+
+    def get_info(self):
         pass
