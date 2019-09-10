bfa4a07091bd7c4511534dac95c2f0ba7e324d8b
Karl-Cui <karlcui174@gmail.com>
2019-09-09 23:54:28 -0400

    Add repetitive testing to test script

--- neurostack/test.py
+++ neurostack/test.py
@@ -3,6 +3,7 @@ import random
 import time
 import json
 
+
 def on_retrieve_prediction_results(*args):
     print(args)
     sid = args[0]
@@ -19,6 +20,7 @@ def on_train_results(*args):
 def print_results(*args):
     print(args[0])
 
+
 # p300 server running on localhost:8001
 socket_client = SocketIO('localhost', 8002)
 socket_client.connect()
@@ -27,14 +29,20 @@ socket_client.connect()
 uuid = None
 timestamp = time.time()
 p300 = 1
-user = "karl"
-password = "cui"
-email = "e@mail"
 
-args = json.dumps({'uuid': uuid, 'timestamp': timestamp})
-socket_client.emit("predict", args, print_results)
-socket_client.wait_for_callbacks(seconds=2)
 
-args = json.dumps({'uuid': uuid, 'timestamp': timestamp, 'p300': p300})
-socket_client.emit("train", args, print_results)
-socket_client.wait_for_callbacks(seconds=2)
+for i in range(50):
+    timestamp = time.time()
+    # p300 = random.choice([0, 1])
+
+    args = json.dumps({'uuid': uuid, 'timestamp': timestamp})
+    socket_client.emit("predict", args, print_results)
+    socket_client.wait_for_callbacks(seconds=2)
+
+    # args = json.dumps({'uuid': uuid, 'timestamp': timestamp, 'p300': p300})
+    # socket_client.emit("train", args, print_results)
+    # socket_client.wait_for_callbacks(seconds=2)
+
+    time.sleep(1)
+
+socket_client.disconnect()
