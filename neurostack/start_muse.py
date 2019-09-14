ba4b21881739f6b91ea8cf3d500b1ebd8f827b30
Karl-Cui <karlcui174@gmail.com>
2019-09-13 23:18:07 -0400

    Start Muse script

--- neurostack/start_muse.py
+++ neurostack/start_muse.py
@@ -9,4 +9,4 @@ muse.neurostack_connect('35.222.93.233', 8001)
 
 # run client as server as well (to allow API for front end)
 muse.initialize_handlers()
-muse.app.run(host='localhost', port=8001)
+muse.app.run(host='localhost', port=8002)
