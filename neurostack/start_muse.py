97e812ad0b23d1aeebff2c6929763a50997db655
Karl-Cui <karlcui174@gmail.com>
2019-09-13 23:17:44 -0400

    README

--- neurostack/start_muse.py
+++ neurostack/start_muse.py
@@ -5,8 +5,8 @@ muse.connect()
 muse.start()
 
 # connect neurostack server
-muse.neurostack_connect('localhost', 8001)
+muse.neurostack_connect('35.222.93.233', 8001)
 
 # run client as server as well (to allow API for front end)
 muse.initialize_handlers()
-muse.app.run(host='localhost', port=8002)
+muse.app.run(host='localhost', port=8001)
--- neurostack/test.py
+++ neurostack/test.py
@@ -26,23 +26,29 @@ socket_client = SocketIO('localhost', 8002)
 socket_client.connect()
 
 # uuid = random.randint(0, 1e10)
-uuid = None
+uuid = 'd6af3d5a-da9f-4199-95d1-750308b0d1aa'
 timestamp = time.time()
 p300 = 1
 
 
-for i in range(50):
+# socket_client.emit('generate_uuid', None, print_results)
+# socket_client.wait_for_callbacks(seconds=1)
+
+
+for i in range(40):
     timestamp = time.time()
-    # p300 = random.choice([0, 1])
+    p300 = random.choice([0, 1])
 
     args = json.dumps({'uuid': uuid, 'timestamp': timestamp})
     socket_client.emit("predict", args, print_results)
     socket_client.wait_for_callbacks(seconds=2)
 
+    time.sleep(1)
+
     # args = json.dumps({'uuid': uuid, 'timestamp': timestamp, 'p300': p300})
     # socket_client.emit("train", args, print_results)
     # socket_client.wait_for_callbacks(seconds=2)
-
-    time.sleep(1)
+    #
+    # time.sleep(1)
 
 socket_client.disconnect()
