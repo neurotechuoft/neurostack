a52bb79e0aa538ffd5451bd402b81764a52df269
Karl Cui <karl.cui@mail.utoronto.ca>
2020-04-06 18:14:08 -0700

    Delete unused / old stuff

--- neurostack/devices/__init__.py
+++ neurostack/devices/__init__.py
@@ -1,4 +1,3 @@
 from devices.device import Device
 from devices.muse import Muse
-from devices.muse2014 import Muse2014
 # from devices.openbci import OpenBCI
--- neurostack/playground/python-socketio-demo/socket_client.py
+++
@@ -1,8 +0,0 @@
-from socketIO_client import SocketIO
-
-if __name__ == '__main__':
-    socket_client = SocketIO("localhost", 8001)
-    socket_client.connect()
-
-    while True:
-        socket_client.emit("data_event", "hello")
